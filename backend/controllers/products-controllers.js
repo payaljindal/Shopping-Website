38e6290c55c5ac34e07a054dac2f51df30cc2f1e
payaljindal <payaljindal05052001@gmail.com>
2020-06-04 23:36:03 +0530

    displayed products on home page basic without pic

--- backend/controllers/products-controllers.js
+++ backend/controllers/products-controllers.js
@@ -1,19 +1,31 @@
 const mongoose = require('mongoose');
-const Products = require('../models/product-model');
+const Product = require('../models/product-model');
 const HttpError = require('../models/http-error');
 
 
 const getProducts = async (req,res,next) => {
-    let products;
-  try{
-    products = await Products.find({});
-  }catch{
-    return next(new HttpError('Could not get products list. Please try again later')
-      );
-  }
+    // let products;
+  // try{
+  //   products = Products.find({});
+  // }catch{
+  //   return next(new HttpError('Could not get products list. Please try again later')
+  //     );
+  // }
 
-  res.status(200).json({ Products: products.map((products) => products.toObject({ getters: true })) });
-  // res.render('shop/index', {title: 'Shopping Cart', products: products} );
+  var count;
+
+  await Product.countDocuments(function (err, c) {
+      count = c;
+  });
+
+  Product.find(function (err, products) {
+      res.render('index', {
+        title: 'Shopping Cart',
+          products: products,
+          count: count
+      });
+  });
+
 };
 
 
