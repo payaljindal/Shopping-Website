0dc12531157ed8ced76278ff795f4c468cb70318
payaljindal <payaljindal05052001@gmail.com>
2020-06-07 10:23:02 +0530

    added post route for user

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const Users = require('../models/user-model');
+const User = require('../models/user-model');
 const HttpError = require('../models/http-error');
 const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
 const Order = require('../models/order-model.js')
 const Cart = require('../models/cart-model.js')
+const { check } = require('express-validator');
 
 const getUsers = async (req,res,next) => {
 
@@ -21,65 +22,132 @@ const getUsers = async (req,res,next) => {
 }
 
 const signup = async (req,res,next) => {
+  console.log("reached");
+
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  // req.checkBody('name', 'Name is required!').notEmpty();
+  // req.checkBody('email', 'Email is required!').isEmail();
+  // req.checkBody('username', 'Username is required!').notEmpty();
+  // req.checkBody('password', 'Password is required!').notEmpty();
+  // req.checkBody('password2', 'Passwords do not match!').equals(password);
+  
   const errors = validationResult(req);
+
   if (!errors.isEmpty()) {
-    return next(
-      new HttpError('Invalid inputs passed, please check your data.', 422),
-    );
+
+// need to display errors
+
+    // console.log(errors[0].msg);
+      res.render('register', {
+          errors: errors,
+          user: null,
+          title: 'Register'
+      });
+  } else {
+      User.findOne({username: username}, function (err, user) {
+          if (err)
+              console.log(err);
+
+          if (user) {
+              req.flash('danger', 'Username exists, choose another!');
+              console.log("exists");
+              res.redirect('/users/register');
+          } else {
+              var user = new User({
+                  name: name,
+                  email: email,
+                  username: username,
+                  password: password,
+                  admin: 0
+              });
+
+              bcrypt.genSalt(10, function (err, salt) {
+                  bcrypt.hash(user.password, salt, function (err, hash) {
+                      if (err)
+                          console.log(err);
+
+                      user.password = hash;
+
+                      user.save(function (err) {
+                          if (err) {
+                              console.log(err);
+                          } else {
+                              req.flash('success', 'You are now registered!');
+                              console.log("succes");
+                              res.redirect('/users/register');
+                          }
+                      });
+                  });
+              });
+          }
+      });
   }
 
-	const{ name, email , password } = req.body;
+
+  // const errors = validationResult(req);
+  // if (!errors.isEmpty()) {
+  //   return next(
+  //     new HttpError('Invalid inputs passed, please check your data.', 422),
+  //   );
+  // }
+
+	// const{ name, email , password } = req.body;
 	
 	
-	let existinguser;
+	// let existinguser;
 
-	try{
-		existinguser = await Users.findOne({email : email });
-	}catch {
-		const error = new HttpError('Some error occured. Please try again later.')
-	}
+	// try{
+	// 	existinguser = await Users.findOne({email : email });
+	// }catch {
+	// 	const error = new HttpError('Some error occured. Please try again later.')
+	// }
 
-	if (existinguser) {
-    const error = new HttpError(
-      'User exists already, please login instead.',
-      422,
-    );
-    return next(error);
-    }
+	// if (existinguser) {
+  //   const error = new HttpError(
+  //     'User exists already, please login instead.',
+  //     422,
+  //   );
+  //   return next(error);
+  //   }
 
-    let hashedPassword;
-	  try {
-	    hashedPassword = await bcrypt.hash(password, 12);
-	  } catch (err) {
-	    const error = new HttpError(
-	      'Could not create user, please try again.',
-	      500,
-	    );
-	    return next(error);
-	  }
+    // let hashedPassword;
+	  // try {
+	  //   hashedPassword = await bcrypt.hash(password, 12);
+	  // } catch (err) {
+	  //   const error = new HttpError(
+	  //     'Could not create user, please try again.',
+	  //     500,
+	  //   );
+	  //   return next(error);
+	  // }
 
-	  const createdUser = new Users({
-	  	name,
-	  	email,
-	  	password : hashedPassword,
-	  	cart : []
-	  });
+	  // const createdUser = new Users({
+	  // 	name,
+	  // 	email,
+	  // 	password : hashedPassword,
+	  // 	// cart : []
+	  // });
 
-	  try {
-      await createdUser.save();
-      } catch (err) {
-      const error = new HttpError(
-      'Signing up failed, please try again later.',
-      500,
-      );
-      return next(error);
-      }
+	  // try {
+    //   await createdUser.save();
+    //   } catch (err) {
+    //   const error = new HttpError(
+    //   'Signing up failed, please try again later.',
+    //   500,
+    //   );
+    //   return next(error);
+    //   }
 
 
-  res.status(201).json({
-    userId: createdUser.id,
-    email: createdUser.email
-  });
+  // res.status(201).json({
+  //   userId: createdUser.id,
+  //   email: createdUser.email
+  // });
 
 
 };
